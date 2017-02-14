a23d4d59cca503a289bcaeb305c54055ad9836fa
Steven Pritchard <steven.pritchard@gmail.com>
2017-02-13 21:40:30 -0600

    Switch to NetworkManager.

--- client.sh
+++ client.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-sudo sed -i -e '/^DEFROUTE=/d;$aDEFROUTE=no' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '/^PEERROUTES=/d;$aPEERROUTES=no' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '/^DEVICE=/d;$aDEVICE=eth0' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '$aGATEWAY=192.168.11.254' /etc/sysconfig/network-scripts/ifcfg-eth1
+sudo sed -i \
+    -e '/^DEFROUTE=/d;$aDEFROUTE=no' \
+    -e '/^PEERROUTES=/d;$aPEERROUTES=no' \
+    -e '/^DEVICE=/d;$aDEVICE=eth0' \
+    -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    /etc/sysconfig/network-scripts/ifcfg-eth0
 
+sudo sed -i \
+    -e '$aGATEWAY=192.168.11.254' \
+    -e '/^DEFROUTE=/d;$aDEFROUTE=yes' \
+    -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    -e '/^BOOTPROTO=/d;$aBOOTPROTO=static' \
+    /etc/sysconfig/network-scripts/ifcfg-eth1
+
+sudo systemctl stop network.service
 sudo systemctl stop NetworkManager.service
-sudo systemctl disable NetworkManager.service
-
 [ -f /var/run/dhclient-eth0.pid ] && sudo pkill dhclient
-sudo ip route delete default
-
-sudo chkconfig network on
-sudo systemctl restart network.service
+sudo systemctl start NetworkManager.service
--- fw-common.sh
+++ fw-common.sh
@@ -14,19 +14,30 @@ fi
 sudo yum -y install keepalived conntrack-tools perl-Data-Dumper
 sudo yum -y install http://ftp.kspei.com/pub/kspei/add-ons/el/6/noarch/genfw-1.47-1.el6.noarch.rpm
 
-sudo sed -i -e '/^DEFROUTE=/d;$aDEFROUTE=no' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '/^PEERROUTES=/d;$aPEERROUTES=no' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '/^DEVICE=/d;$aDEVICE=eth0' /etc/sysconfig/network-scripts/ifcfg-eth0
-sudo sed -i -e '$aGATEWAY=192.168.10.1' /etc/sysconfig/network-scripts/ifcfg-eth1
+sudo sed -i \
+    -e '/^DEFROUTE=/d;$aDEFROUTE=no' \
+    -e '/^PEERROUTES=/d;$aPEERROUTES=no' \
+    -e '/^DEVICE=/d;$aDEVICE=eth0' \
+    -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    /etc/sysconfig/network-scripts/ifcfg-eth0
 
+sudo sed -i \
+    -e '$aGATEWAY=192.168.10.1' \
+    -e '/^DEFROUTE=/d;$aDEFROUTE=yes' \
+    -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    -e '/^BOOTPROTO=/d;$aBOOTPROTO=static' \
+    /etc/sysconfig/network-scripts/ifcfg-eth1
+
+sudo sed -i \
+    -e '/^DEFROUTE=/d;$aDEFROUTE=no' \
+    -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    -e '/^BOOTPROTO=/d;$aBOOTPROTO=static' \
+    /etc/sysconfig/network-scripts/ifcfg-eth2
+
+sudo systemctl stop network.service
 sudo systemctl stop NetworkManager.service
-sudo systemctl disable NetworkManager.service
-
 [ -f /var/run/dhclient-eth0.pid ] && sudo pkill dhclient
-sudo ip route delete default
-
-sudo chkconfig network on
-sudo systemctl restart network.service
+sudo systemctl start NetworkManager.service
 
 sudo $vagrantsync/keepalived.conf.sh "$@"
 
