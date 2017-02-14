4e81f649f787aa67fb03a7146617c2ed618abcd4
Steven Pritchard <steven.pritchard@gmail.com>
2017-02-14 09:25:18 -0600

    Switch to firewalld.

--- fw-common.sh
+++ fw-common.sh
@@ -11,14 +11,30 @@ else
     exit 1
 fi
 
-sudo yum -y install keepalived conntrack-tools perl-Data-Dumper
-sudo yum -y install http://ftp.kspei.com/pub/kspei/add-ons/el/6/noarch/genfw-1.47-1.el6.noarch.rpm
+sudo yum -y install keepalived conntrack-tools
+
+# For demo/diagnostic purposes
+sudo yum -y install iptraf-ng tcpdump
+
+sudo systemctl enable firewalld.service
+sudo systemctl start firewalld.service
+
+sudo firewall-cmd --zone=external \
+  --add-rich-rule='rule protocol value="vrrp" accept' --permanent
+sudo firewall-cmd --zone=internal \
+  --add-rich-rule='rule protocol value="vrrp" accept' --permanent
+
+sudo firewall-cmd --permanent --direct \
+    --add-rule ipv4 nat POSTROUTING 0 -o eth1 -j SNAT --to 192.168.10.254
+
+sudo firewall-cmd --reload
 
 sudo sed -i \
     -e '/^DEFROUTE=/d;$aDEFROUTE=no' \
     -e '/^PEERROUTES=/d;$aPEERROUTES=no' \
     -e '/^DEVICE=/d;$aDEVICE=eth0' \
     -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
+    -e '/^ZONE=/d;$aZONE=trusted' \
     /etc/sysconfig/network-scripts/ifcfg-eth0
 
 sudo sed -i \
@@ -26,12 +42,14 @@ sudo sed -i \
     -e '/^DEFROUTE=/d;$aDEFROUTE=yes' \
     -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
     -e '/^BOOTPROTO=/d;$aBOOTPROTO=static' \
+    -e '/^ZONE=/d;$aZONE=external' \
     /etc/sysconfig/network-scripts/ifcfg-eth1
 
 sudo sed -i \
     -e '/^DEFROUTE=/d;$aDEFROUTE=no' \
     -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
     -e '/^BOOTPROTO=/d;$aBOOTPROTO=static' \
+    -e '/^ZONE=/d;$aZONE=internal' \
     /etc/sysconfig/network-scripts/ifcfg-eth2
 
 sudo systemctl stop network.service
@@ -48,10 +66,6 @@ sudo $vagrantsync/conntrackd.conf.sh
 
 sudo systemctl enable conntrackd
 sudo systemctl start conntrackd
-
-sudo cp -fv $vagrantsync/*.rules /etc/sysconfig/genfw/rules.d/
-sudo chkconfig firewall on
-sudo systemctl start firewall.service
 
 sudo cp -fv $vagrantsync/ip_forward.conf /etc/sysctl.d/
 sudo sysctl -w net.ipv4.ip_forward=1
